ff92afef34eee1017b9341c5dc35d2e4bde0ddfc
hughesc13 <60516530+hughesc13@users.noreply.github.com>
2024-11-21 05:11:03 -0500

    headless chrome

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -36,9 +36,15 @@ public class D3Test {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
+   // driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
+
+
   }
   @After
   public void tearDown() {
